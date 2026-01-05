f299fcb6daccf1f3f18da43dc1d910a4185ae6ec
dextermyles <dexter.brock@gmail.com>
2026-01-05 11:46:30 -0500

    markers

--- src/structures/MapMarkers.js
+++ src/structures/MapMarkers.js
@@ -340,6 +340,7 @@ class MapMarkers {
 
             vendingMachine.id = marker.id;
             vendingMachine.location = pos;
+            vendingMachine.sellOrders = marker.sellOrders;
         }
     }
 
