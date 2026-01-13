f29931e447a7c96e8d10595e9c8bd7660e0a761e
dextermyles <dexter.brock@gmail.com>
2026-01-13 10:18:27 -0500

    answer tweak

--- src/structures/Ai.js
+++ src/structures/Ai.js
@@ -152,7 +152,7 @@ class Ai {
                     + "Assume all questions about Rust refer to the PC game developed by Facepunch (https://rust.facepunch.com/), not the programming language.\n"
                     + "Rust changes can be found at https://rust.facepunch.com/changes" + "\n"
                     + "The only exception is if the user asks a gambling question about Casino games in Rust (black jack, slot machine, big wheel).\n"
-                    + "Provide a concise final answer.\n"
+                    + "Provide a concise final answer, do not show the equation only the total (when answering a raid question).\n"
                     + "Use Plain Text in your output, do not use any special characters that require encoding.\n"
             },
             { role: 'user', content: userQuery }
